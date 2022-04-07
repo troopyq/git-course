1e5783caaeffa7facea41553b430981f50160cf7
Dima Nemkov <nemkov03@mail.ru>
2022-04-07 09:23:26 +0500

    добавил функцию getname

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-document.body.innerHTML = '<h2>H2 title</h2>'
+
+let a = 1233
+
+console.log('434')
+
+console.log(223)
+
+let d = 2333
+let tree = {} /* this tree objects */
 
 console.log(123)
 
+console.log(29002)
+
 console.log('test 2')
+
+console.log('Разрабатываю функционал')
+
+function getname() {
+  return 'dima'
+}
